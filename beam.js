bea910793ed6702c724812b7230220087335000a
tehlordvortex <hadokoda@yahoo.com>
2017-08-23 15:05:55 +0100

    inital beam implementation

---
+++ beam.js
@@ -0,0 +1,18 @@
+class Node {
+	constructor(value, children) {
+		this.value = value
+		this.children = children
+	}
+}
+function beamSearch(rootNode, isGoal, determineCost, beamWidth) {
+	let underConsideration = [rootNode]
+	while (underConsideration.length > 0) {
+		underConsideration.forEach((node) => {
+			if(isGoal(node)) return node
+			underConsideration = node.children
+									.sort((a, b) => determineCost(a) > determineCost(b))
+									.slice(0, beamWidth)
+		})
+	}
+	return null // nothing
+}
